d287735f5c3c168a2aca44b5034902d93f0873ff
Andon Mefford <andonmefford@outlook.com>
2023-12-14 18:47:36 -0700

    Update code.js

--- code.js
+++ code.js
@@ -9,38 +9,18 @@ function mergesort(arr) {
   }
   return arr;
 }
+
 function merge(arr, left, mid, right) {
+  let i, j, k;
   const n1 = mid - left + 1;
   const n2 = right - mid;
-  const leftArr = new Array(n1);
-  const rightArr = new Array(n2);
-  for (let i = 0; i < n1; ++i) {
-    leftArr[i] = arr[left + i];
-  }
-  for (let j = 0; j < n2; ++j) {
-    rightArr[j] = arr[mid + 1 + j];
-  }
-  let i = 0;
-  let j = 0;
-  let k = left;
-  while (i < n1 && j < n2) {
-    if (leftArr[i] <= rightArr[j]) {
-      arr[k] = leftArr[i];
-      i++;
-    } else {
-      arr[k] = rightArr[j];
-      j++;
+  for (i = 0; i < n1; i++) {
+    let current = arr[left + i];
+    k = left + i;
+    for (j = mid + 1; j <= right && arr[j] < current; j++) {
+      arr[k] = arr[j];
+      k = j;
     }
-    k++;
-  }
-  while (i < n1) {
-    arr[k] = leftArr[i];
-    i++;
-    k++;
-  }
-  while (j < n2) {
-    arr[k] = rightArr[j];
-    j++;
-    k++;
+    arr[k] = current;
   }
 }
